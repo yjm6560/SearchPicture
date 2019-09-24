d4ed15a93cfb3c74f22436341f307e0b2751cbed
yjm6560 <yjm7558@nate.com>
2019-09-24 15:41:55 +0900

    Implement CreateHierarchyTree class

--- HierarchyTree/CreateHierarchyTree.py
+++ HierarchyTree/CreateHierarchyTree.py
@@ -1,5 +1,4 @@
 from anytree import Node, RenderTree
-from anytree.exporter import DotExporter
 from HierarchyTree.FilePath import filePathGetter
 
 class HierarchyTree:
@@ -18,19 +17,46 @@ class HierarchyTree:
 
     def makeTree(self):
         self.makeContent()
-        print("Not implemented")
+        self.node_set.append(Node(f'node_{0}', data = self.content[0][1]))
+        self.makeTree_sub(0)
+
+    def makeTree_sub(self, nodeNum):
+        while True:
+            if (len(self.content) == len(self.node_set)) or (self.content[nodeNum][0] > self.content[len(self.node_set)][0] - 1):
+                return
+            elif self.content[nodeNum][0] == self.content[len(self.node_set)][0]- 1:
+                self.node_set.append(Node(f'node_{len(self.node_set)}', parent = self.node_set[nodeNum], data = self.content[len(self.node_set)][1]))
+            elif self.content[nodeNum][0] < self.content[len(self.node_set)][0] - 1:
+                self.makeTree_sub(len(self.node_set) - 1)
 
     def searchKeyword(self, keyword):
-        print("Not implemented")
+        for row in RenderTree(self.node_set[0]):
+            pre, fill, node = row
+            if keyword in node.data:
+                return self.getRelatedNodes(node)
 
     def getRelatedNodes(self, node):
-        print("Not implemented")
+        result = []
+        result.append(self.getParents(node))
+        result.append(self.getChildren(node))
+        return result
 
     def getChildren(self, node):
-        print("Not implemented")
+        children = []
+        for row in RenderTree(node):
+            pre, fill, node = row
+            children.append(node.data)
+        return children
 
     def getParents(self, node):
-        print("Not implemented")
+        parents = []
+        while True:
+            if node.parent:
+                parents.append(node.parent.data)
+                node = node.parent
+            else:
+                break
+        return parents
 
     def showTree(self):
         print("==" * 20)
@@ -42,4 +68,7 @@ class HierarchyTree:
 if __name__ == "__main__":
     ht = HierarchyTree()
     ht.makeTree()
-    print(ht.content)
+    print(f'keyword : 패션의류 result : {ht.searchKeyword("패션의류")}')
+    print(f'keyword : 러닝 result : {ht.searchKeyword("러닝")}')
+    print(f'keyword : 신발 result : {ht.searchKeyword("신발")}')
+
