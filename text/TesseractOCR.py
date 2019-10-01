fce33e5337e0b95cb0df9c7886d02e9b2163cde2
easy <0319easy@gmail.com>
2019-10-01 21:56:03 +0900

    Modify FindTextRegion.py and TesseractOCR.py

--- text/TesseractOCR.py
+++ text/TesseractOCR.py
@@ -1,5 +1,7 @@
 #tesseract ocr
 import pytesseract
+import FindTextRegion
+import cv2
 
 '''
 class는 이미지가 저장된 경로와 이미지의 텍스트 부분이 잘린 조각 이미지들(list)이 넘어오면
@@ -11,16 +13,34 @@ test 필요
 '''
 
 class TesseractOCR:
-    def __init__(self, imagepath, images):
+    def __init__(self, image):
         #images는 numpy.ndarray들의 list여야한다
         pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract' #tesseract가 저장된 경로 입력
-        self.imagepath = imagepath
-        self.images = images
+        #image는 cv2.imread()로 읽은 사진
+        self.image = image
 
     def ocr(self):
         result = ''
-        for img in self.images:
+        for img in self.text_region_list:
             string = pytesseract.image_to_string(img, lang='kor')
             result = result + string + '\n'
 
         return result
+
+    def findTextOnImage(self):
+        findtextregion = FindTextRegion.FindTextRegion(self.image)
+        self.text_region_list = findtextregion.findTextRegion(5, 5, 15, 3, 5, 5, 1)
+        text = self.ocr()
+
+        return text
+
+
+#text
+'''
+if __name__ == "__main__":
+    a = cv2.imread('1.jpg')
+    b = TesseractOCR(a)
+    result = b.findTextOnImage()
+
+    print(result)
+'''
