eddf510c4215daaef2c004f9437d1a9f0634139a
yjm6560 <yjm7558@nate.com>
2019-10-15 19:51:23 +0900

    Modify output format

--- Launcher.py
+++ Launcher.py
@@ -9,7 +9,11 @@ Launcher
 3. test
 '''
 #TODO : 지금은 한 번 돌고 끝내는 상황임. 무한으로 돌게 고쳐야됨
+
+DEBUG = True
+
 if __name__ == "__main__":
+
     user_1 = "yjm6560"
     user_2 = "easy"
     user = user_2
@@ -23,18 +27,28 @@ if __name__ == "__main__":
     IC = ImageClassifier.ImageClassifier(user)
     data_list = IC.imagesClassify()
 
+    #classify images and insert into database
+    print("inserted image list")
     for data in data_list:
-        print(data)
+        print(data[1])
         logger.insertTextyPhoto(data[0], data[1], data[2], data[3])
     #test example
     tag_data = ["cat", "dog", "truck"]
     text_tag = ["second","first"]
 
-    print("TAG SEARCH")
+    #search by tag
+    print("\nTAG SEARCH")
     for tag in tag_data:
-        print(tag, " ", logger.getPhotoByTag(tag))
-
-    print("TEXT SEARCH")
+        print("SEARCH TAG : ", tag)
+        ret = logger.getPhotoByTag(tag)
+        for dat in ret:
+            print("\t",dat)
+    #search by text
+    print("\nTEXT SEARCH")
     for text in text_tag:
-        print(text, " ", logger.getPhotoByText(text))
+        print("SEARCH TEXT : ", text)
+        ret = logger.getPhotoByText(text)
+        for dat in ret:
+            print("\t", dat)
+
 
