5f3b1bbada100c69ac42d864c36e64eb2755d441
easy <0319easy@gmail.com>
2019-11-19 02:30:29 +0900

    Modify analysis.js and analysis.ejs

--- web/routes/analysis/analysis.js
+++ web/routes/analysis/analysis.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var pythonShell = require('python-shell');
+var path = require('path');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 var analysis = {
     show : function(req, res) {
-        res.render('analysis/analysis');
+        var scriptPath = path.resolve('..', 'Launcher.py');
+        console.log(scriptPath);
+        var pythonOptions = {
+            mode: 'text',
+            pythonPath: '',
+            pythonOptions: [],
+            scriptPath: '',
+            args: []
+        };
+
+        pythonShell.PythonShell.run(scriptPath, pythonOptions, function(err, results) {
+            if (err) {
+                console.error(err);
+            }
+
+            console.log('results: %j', results);
+
+            res.render('analysis/analysis', {results:results});
+        });
     }
 };
 
