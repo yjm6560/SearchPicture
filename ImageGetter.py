970080844cec61331893a8cc6372267a73da10af
yjm6560 <yjm7558@nate.com>
2019-11-20 03:29:08 +0900

    Add jpeg format in ImagetGetter

--- ImageGetter.py
+++ ImageGetter.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2
 import sys
 import os
+
+import Launcher
 from FilePath import FilePathGetter
 '''
 ImageGetter
@@ -21,9 +23,11 @@ class ImageGetter:
 
     def getFileList(self, logger):
         file_name_list = os.listdir(self.path_dir)
-        path_list = [self.path_dir + '\\' + file_name for file_name in file_name_list if ".jpg" in file_name or ".png" in file_name]
+        path_list = [self.path_dir + '\\' + file_name for file_name in file_name_list if ".jpg" in file_name or ".png" or ".jpeg" in file_name in file_name]
         path_list_in_db = logger.getAllPath()
         path_list_in_db = [path[0] for path in path_list_in_db]
+        if Launcher.DEBUG:
+            print(path for path in path_list if path not in path_list_in_db)
         return [path for path in path_list if path not in path_list_in_db]
 
 if __name__ == "__main__":
